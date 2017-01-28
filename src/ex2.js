656fb83f2b816ef40c86e167b754d9971578a5f6
sr <sr@rs.com>
2017-01-28 22:10:29 +0200

    Use event listeners registration instead of onclick property

--- src/ex2.js
+++ src/ex2.js
@@ -9,10 +9,10 @@ const PASSWORD_LABEL_ID = 'password-label'
 const USERNAME_INPUT_ID = 'username'
 const PASSWORD_INPUT_ID = 'password'
 const ERRORS_SECTION_ID = 'errors'
+const LOGIN_BUTTON_ID = 'login-button'
+const CLOSE_BUTTON_ID = 'close-button'
 
-function hideLoginForm() {
-	document.getElementById(LOGIN_FORM_ID).style.visibility = 'hidden'
-}
+
 
 function isValidPassword(pass) {
 	return pass.length >= MIN_PASSWORD_LEN && !pass.includes(' ')
@@ -87,3 +87,11 @@ function login(){
 		setErr(err)
 	}	
 }
+
+function hideLoginForm() {
+	document.getElementById(LOGIN_FORM_ID).style.visibility = 'hidden'
+}
+
+// Register event listeners for close button and login button
+document.getElementById(LOGIN_BUTTON_ID).addEventListener("click", login)
+document.getElementById(CLOSE_BUTTON_ID).addEventListener("click", hideLoginForm)
