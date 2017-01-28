196d7cb35baab3b7ee1a870ca4ff9ffaa3f4a26c
sr <sr@rs.com>
2017-01-28 19:36:28 +0200

    Add error messages display

--- src/ex2.js
+++ src/ex2.js
@@ -13,12 +13,47 @@ function isValidUsername(username) {
 	});
 }
 
-function login(){
-	var pass = document.getElementById("password").value
-	var username = document.getElementById("username").value
-	console.log(pass, username)
-	if(isValidPassword(pass) && isValidUsername(username)) {
-		// print a success message instead of login form
-		document.getElementById("login-form").innerHTML="<h1>success</h1>"
+function verify(username, pass) {
+	var err = ""
+	
+	if(!isValidUsername(username)) {
+		err += "*Invalid username format: \
+			username should should contain only\
+			small latters,digits,dash and lodash"
 	}
+	
+	if(!isValidPassword(pass)) {
+		err += "*Invalid password format - password \
+			should contain at least 8 characters \
+			and no backspace characters"
+	}
+	
+	return err;
+}
+
+function setErr(err) {
+	document.getElementById("errors").innerHTML = "<font color=\"red\">" + err + "</font>"
+}
+
+function clearErrSection() {
+	setErr("")
+}
+
+function login(){
+	clearErrSection();
+	
+	var username = document.getElementById("username").value
+	var pass = document.getElementById("password").value
+	
+	var err = verify(username, pass)
+	if(!err) {
+		document.getElementById("login-form").innerHTML="<h1>success</h1>"
+	} else {
+		setErr(err)
+	}
+	
+	// if(isValidPassword(pass) && isValidUsername(username)) {
+		// // print a success message instead of login form
+		// document.getElementById("login-form").innerHTML="<h1>success</h1>"
+	// }
 }
