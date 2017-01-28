a41b4bf3e3ba27d80abf6191dc4c7de59b8c8673
sr <sr@rs.com>
2017-01-28 20:46:01 +0200

    Refactor some more

--- src/ex2.js
+++ src/ex2.js
@@ -1,25 +1,25 @@
 // Label color when input is invalid\valid(or unverified yet)
-const ERR_COLOR = "red"
-const VALID_COLOR = "black"
+const ERR_COLOR = 'red'
+const VALID_COLOR = 'black'
 
 const MIN_PASSWORD_LEN = 8
-const USERNAME_LABEL_ID = "username-label"
-const LOGIN_FORM_ID = "login-form"
-const PASSWORD_LABEL_ID = "password-label"
-const USERNAME_INPUT_ID = "username"
-const PASSWORD_INPUT_ID = "password"
+const USERNAME_LABEL_ID = 'username-label'
+const LOGIN_FORM_ID = 'login-form'
+const PASSWORD_LABEL_ID = 'password-label'
+const USERNAME_INPUT_ID = 'username'
+const PASSWORD_INPUT_ID = 'password'
 
 function hideLoginForm() {
-	document.getElementById(LOGIN_FORM_ID).style.visibility = "hidden"
+	document.getElementById(LOGIN_FORM_ID).style.visibility = 'hidden'
 }
 
 function isValidPassword(pass) {
-	return pass.length >= MIN_PASSWORD_LEN && !pass.includes(" ")
+	return pass.length >= MIN_PASSWORD_LEN && !pass.includes(' ')
 }
 
 function isValidUsername(username) {
-	return username.length && username.split("").every(function(e) { 
-		return RegExp("[a-z0-9_\-]").test(e)   
+	return username.length && username.split('').every(function(e) { 
+		return RegExp('[a-z0-9_\-]').test(e)   
 	});
 }
 
@@ -31,33 +31,34 @@ function unmarkLabel(labelId) {
 	document.getElementById(labelId).style.color = VALID_COLOR
 }
 
+
 function verify(username, pass) {
-	var err = ""
+	var err = ''
 	
 	if(!isValidUsername(username)) {
 		markLabel(USERNAME_LABEL_ID);
-		err += "*Invalid username format: \
+		err += '*Invalid username format: \
 			username should should contain only\
-			small latters,digits,dash and lodash"
+			small latters,digits,dash and lodash'
 	}
 	
 	if(!isValidPassword(pass)) {
 		markLabel(PASSWORD_LABEL_ID);
-		err += "*Invalid password format - password \
+		err += '*Invalid password format - password \
 			should contain at least 8 characters \
-			and no backspace characters"
+			and no backspace characters'
 	}
 	
 	return err;
 }
 
 function setErr(err) {
-	document.getElementById("errors").innerHTML = 
-		"<font color=\"red\">" + err + "</font>"
+	document.getElementById('errors').innerHTML = 
+		'<font color=\"red\">' + err + '</font>'
 }
 
 function clearErrors() {
-	setErr("")
+	setErr('')
 	unmarkLabel(USERNAME_LABEL_ID)
 	unmarkLabel(PASSWORD_LABEL_ID)
 }
@@ -70,7 +71,7 @@ function login(){
 	
 	var err = verify(username, pass)
 	if(!err) {
-		document.getElementById(LOGIN_FORM_ID).innerHTML="<h1>success</h1>"
+		document.getElementById(LOGIN_FORM_ID).innerHTML='<h1>success</h1>'
 	} else {
 		setErr(err)
 	}	
