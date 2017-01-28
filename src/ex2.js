c0ebbf89550db683695d00a44ab4098d9012345e
sr <sr@rs.com>
2017-01-28 18:46:48 +0200

    Add login details verification

--- src/ex2.js
+++ src/ex2.js
@@ -1,3 +1,24 @@
-function hideLoginForm(e) {
-	document.getElementById("login-form").style.visibility = "hidden";
+function hideLoginForm() {
+	document.getElementById("login-form").style.visibility = "hidden"
+}
+
+
+function isValidPassword(pass) {
+	return pass.length >= 8 && !pass.includes(" ")
+}
+
+function isValidUsername(username) {
+	return username.length && username.split("").every(function(e) { 
+		return RegExp("[a-z0-9_\-]").test(e)   
+	});
+}
+
+function login(){
+	var pass = document.getElementById("password").value
+	var username = document.getElementById("username").value
+	console.log(pass, username)
+	if(isValidPassword(pass) && isValidUsername(username)) {
+		// print a success message instead of login form
+		document.getElementById("login-form").innerHTML="<h1>success</h1>"
+	}
 }
